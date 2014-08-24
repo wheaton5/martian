a735589f83ac69e281c47407fa0f114883ef9cc2
ablewhiskey <alex@10xtechnologies.com>
2014-08-23 18:58:19 -0700

    make getlena.sh use LENA_AUTH_TOKEN

--- marsoc/getlena.sh
+++ marsoc/getlena.sh
@@ -4,6 +4,6 @@
 # Usage:
 #     getlena.sh <LENA_AUTH_TOKEN>
 #
-curl --insecure --header "Authorization: Token $1" https://$LENA_HOST/seq_results/api/samples/ -o temp.json
+curl --insecure --header "Authorization: Token $LENA_AUTH_TOKEN" https://$LENA_HOST/seq_results/api/samples/ -o temp.json
 node -e "console.log(JSON.stringify(JSON.parse(require('fs').readFileSync(process.argv[1])), null, 4));" temp.json > lena.json
 rm temp.json
