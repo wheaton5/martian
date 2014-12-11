e459c26dd2218e6a1355413fa040ab7943f9ebf6
Ryan Wilson <ryan@10xtechnologies.com>
2014-12-10 16:21:38 -0800

    Wipe all analysis pipelines

--- scripts/wipe_flowcell.py
+++ scripts/wipe_flowcell.py
@@ -7,12 +7,14 @@ import shutil
 import subprocess
 import sys
 
+def check_permission(path):
+    if not os.access(path, os.W_OK | os.F_OK):
+        print "Permission denied: cannot write %s" % path
+        sys.exit(1)
+
 def check_path(path):
     if not os.path.exists(path):
         print "%s does not exist" % path
-        sys.exit(1)
-    if not os.access(path, os.W_OK | os.F_OK):
-        print "Permission denied: cannot write %s" % path
         sys.exit(1)
 
 def check_env(var):
@@ -24,17 +26,16 @@ def check_env(var):
 
 def get_pipestances(path, name):
     pipestances = []
-    if os.path.exists(path):
-        for filename in os.listdir(path):
-            sample_id =os.path.basename(filename)
-            if not sample_id.isdigit():
-                print "WARNING: %s contains non-integer sample ID %s. Exiting..." % (path, sample_id)
-                sys.exit(1)
-            pipestances.append("ID.%s.%s" % (sample_id, name))
+    for sample_id in os.listdir(path):
+        if not sample_id.isdigit():
+            print "WARNING: %s contains non-integer sample ID %s. Exiting..." % (path, sample_id)
+            sys.exit(1)
+        pipestances.append("ID.%s.%s" % (sample_id, name))
     return pipestances
 
-parser = argparse.ArgumentParser(description="Wipe analyzer and phaser pipestances from a flowcell")
+parser = argparse.ArgumentParser(description="Wipe analysis pipestances from a flowcell")
 parser.add_argument("flowcell", help="flowcell ID")
+parser.add_argument("--delete", action="store_true", help="Actually delete analysis pipestances")
 args = parser.parse_args()
 
 pipestances_path = check_env("MARSOC_PIPESTANCES_PATH")
@@ -42,21 +43,31 @@ cache_path = check_env("MARSOC_CACHE_PATH")
 
 cache_file = os.path.join(cache_path, "pipestances")
 fc_path = os.path.join(pipestances_path, args.flowcell)
-analyzer_path = os.path.join(fc_path, "ANALYZER_PD")
-phaser_path = os.path.join(fc_path, "PHASER_SVCALLER_PD")
 
 check_path(cache_file)
 check_path(fc_path)
 
-pipestances = get_pipestances(analyzer_path, "ANALYZER_PD")
-pipestances += get_pipestances(phaser_path, "PHASER_SVCALLER_PD")
+pipestances = []
+for pipeline_type in os.listdir(fc_path):
+    path = os.path.join(fc_path, pipeline_type)
+    if os.path.isdir(path) and pipeline_type != "BCL_PROCESSOR_PD":
+        pipestances += get_pipestances(path, pipeline_type)
 
 if not pipestances:
-    print "No analyzer or phaser pipestances for flowcell %s found" % args.flowcell
+    print "No analysis pipestances for flowcell %s found" % args.flowcell
+    sys.exit(0)
+else:
+    print "Analysis pipestances for flow cell %s:\n %s" % (args.flowcell, "\n".join(pipestances))
+
+if not args.delete:
     sys.exit(0)
 
-answer = raw_input("Are you sure you want to delete flowcell %s? (yes|no)" % args.flowcell)
+check_permission(cache_file)
+check_permission(fc_path)
+
+answer = raw_input("Are you sure you want to delete flowcell %s? (yes|no)\n" % args.flowcell)
 if answer.lower().strip() != "yes":
+    print "Not wiping flowcell %s" % args.flowcell
     sys.exit(0)
 
 # Stop MARSOC
@@ -73,8 +84,9 @@ with open(cache_file, 'w') as f:
     f.write(json.dumps(cache, indent=4))
 
 # Remove pipestances
-for path in [analyzer_path, phaser_path]:
-    if os.path.exists(path):
+for pipeline_type in os.listdir(fc_path):
+    path = os.path.join(fc_path, pipeline_type)
+    if pipeline_type != "BCL_PROCESSOR_PD":
         shutil.rmtree(path)
 
 # Start MARSOC
