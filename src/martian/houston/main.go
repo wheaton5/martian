6f88557c7083567dd7f11aa9255fecc73b33c881
Alex Wong <alex@10xtechnologies.com>
2015-08-09 18:12:20 -0700

    Add customer upload notification emails
    - refactor out download object

--- src/martian/houston/main.go
+++ src/martian/houston/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"martian/core"
+	"martian/manager"
 	_ "os"
 	"path"
 	_ "path/filepath"
@@ -27,21 +28,35 @@ Options:
 
 	env := core.EnvRequire([][]string{
 		{"HOUSTON_PORT", ">2000"},
+		{"HOUSTON_INSTANCE_NAME", "displayed_in_ui"},
 		{"HOUSTON_BUCKET", "s3_bucket"},
 		{"HOUSTON_LOG_PATH", "path/to/houston/logs"},
 		{"HOUSTON_DOWNLOAD_PATH", "path/to/houston/downloads"},
 		{"HOUSTON_STORAGE_PATH", "path/to/houston/storage"},
+		{"HOUSTON_EMAIL_HOST", "smtp.server.local"},
+		{"HOUSTON_EMAIL_SENDER", "email@address.com"},
+		{"HOUSTON_EMAIL_RECIPIENT", "email@address.com"},
 	}, true)
 
 	core.LogTee(path.Join(env["HOUSTON_LOG_PATH"], time.Now().Format("20060102150405")+".log"))
 
 	//uiport := env["HOUSTON_PORT"]
+	instanceName := env["HOUSTON_INSTANCE_NAME"]
 	bucket := env["HOUSTON_BUCKET"]
 	dlPath := env["HOUSTON_DOWNLOAD_PATH"]
 	stPath := env["HOUSTON_STORAGE_PATH"]
+	emailHost := env["HOUSTON_EMAIL_HOST"]
+	emailSender := env["HOUSTON_EMAIL_SENDER"]
+	emailRecipient := env["HOUSTON_EMAIL_RECIPIENT"]
 
-	dl := NewDownloadManager(bucket, dlPath, stPath)
+	// Mailer
+	mailer := manager.NewMailer(instanceName, emailHost, emailSender,
+		emailRecipient, false)
+
+	// Downloader
+	dl := NewDownloadManager(bucket, dlPath, stPath, mailer)
 	dl.StartDownloadLoop()
+
 	//pipestancesPaths := strings.Split(env["HOUSTON_PIPESTANCES_PATH"], ":")
 
 	// Compute MRO path.
