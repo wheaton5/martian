655e00b398f90faaca905afc2c9fd4e2b5bf4ccd
Houston <houston@houston.fuzzplex.com>
2015-08-26 22:27:38 -0700

    Add BC on whitelist and BC qual metrics

--- web/houston/client/houston.js
+++ web/houston/client/houston.js
@@ -18,9 +18,9 @@
   app.controller('MartianRunCtrl', function($scope, $http, $interval) {
     $scope.pstances = null;
     return $http.get('/api/get-submissions').success(function(data) {
-      var d, _i, _len;
-      for (_i = 0, _len = data.length; _i < _len; _i++) {
-        d = data[_i];
+      var d, i, len;
+      for (i = 0, len = data.length; i < len; i++) {
+        d = data[i];
         d.display_date = d.date.substring(0, 10);
         d.display_domain = d.source.split("@")[0];
         d.display_user = d.source.split("@")[1];
