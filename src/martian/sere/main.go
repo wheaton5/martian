6880884e033a8171ec5f747342bbaafb5dd64382
David Stafford <dstaff@10xgenomics.com>
2016-06-06 11:57:25 -0700

    Add --vdrlist to allow custom VDR rules on the command line

--- src/martian/sere/main.go
+++ src/martian/sere/main.go
@@ -183,7 +183,7 @@ Options:
 	// Runtime
 	rt := core.NewRuntimeWithCores(jobMode, vdrMode, profileMode, martianVersion,
 		-1, -1, reqMemPerCore, maxJobs, jobFreqMillis, stackVars, zip,
-		skipPreflight, enableMonitor, debug, false)
+		skipPreflight, enableMonitor, debug, false, []string{})
 
 	// Mailer
 	mailer := manager.NewMailer(instanceName, emailHost, emailSender,
