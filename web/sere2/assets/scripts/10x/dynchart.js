19ba459eacf9dbc61e773e58e58b33026b341cb2
David Stafford <dstaff@10xgenomics.com>
2016-06-23 13:56:24 -0700

    Switch to a unified page layout Enable click-to-compare.

--- web/sere2/assets/scripts/10x/dynchart.js
+++ web/sere2/assets/scripts/10x/dynchart.js
@@ -4,34 +4,71 @@
 
 var global_chart;
 var global_table;
+var global_table_data;
+var global_compare;
 
 function main() {
 	console.log("HELLO WORLD"); 
 	google.charts.load('current', {'packages':['corechart', 'table']});
 	google.charts.setOnLoadCallback(function() {
 
-		if (document.getElementById('chart1')){
-			global_chart= new google.visualization.LineChart(document.getElementById('chart1'));
-		}
-		if (document.getElementById('table1')){
-			global_table = new google.visualization.Table(document.getElementById('table1'));
-		}
+		global_chart= new google.visualization.LineChart(document.getElementById('plot1'));
+		global_table = new google.visualization.Table(document.getElementById('table1'));
+		global_compare = new google.visualization.Table(document.getElementById('compare1'));
 
 	});
+	
+	$("#table").hide();
+	$("#compare").hide();
+	$("#plot").hide();
+
 
 
 }
 
+function pickwindow(w) {
+	console.log(w)
+	$("#table").hide();
+	$("#compare").hide();
+	$("#plot").hide();
+
+	$("#" + w).show();
+
+	if (w == "compare") {
+		compare_update();
+	}
+
+	if (w == "table") {
+		table_update();
+	}
+
+	if (w == "plot") {
+
+	}
+}
+
 function compare_update() {
-	var url = "/api/compare?base=" + document.getElementById("idold").value +
+
+	var selected = global_table.getSelection();
+	console.log(selected)
+	var idold = get_data_at_row(global_table_data, "test_reports.id", selected[0].row);
+	var idnew= get_data_at_row(global_table_data, "test_reports.id", selected[1].row);
+
+	
+	/*var url = "/api/compare?base=" + document.getElementById("idold").value +
 		"&new=" + document.getElementById("idnew").value +
 		"&metrics_def=met1.json"
+		*/
+	var url = "/api/compare?base=" + idold + 
+		"&new=" + idnew + 
+		"&metrics_def=met1.json"
 	
+	console.log(url)
 	$.getJSON(url, function(data) {
 		console.log(data);
 		var gdata = google.visualization.arrayToDataTable(data.ChartData);
 		var options = {};
-		global_table.draw(gdata, options)
+		global_compare.draw(gdata, options)
 
 
 	})
@@ -43,16 +80,15 @@ function table_update() {
 	var url = "/api/plot?where=&columns=test_reports.id,SHA,userid,finishdate,sampleid,comments"
 
 	$.getJSON(url, function(data) {
+		global_table_data = data;
 		console.log(data);
 		var gdata = google.visualization.arrayToDataTable(data.ChartData);
 		var options = {};
 		global_table.draw(gdata, options)
-
-
 	})
-
-
 }
+
+
 function chart_update() {
 	var x = document.getElementById("chartx");
 	var y = document.getElementById("charty");
@@ -75,10 +111,20 @@ function chart_update() {
 		global_chart.draw(gdata, options);
 
 	})
-
 }
 
+function get_data_at_row(data, columnname, rownumber) {
+	var labels = data.ChartData[0];
 
+	var index;
+	for (var i = 0; i < labels.length; i++) {
+		if (labels[i] = columnname) {
+			index = i;
+			break;
+		}
+	}
+
+	return data.ChartData[rownumber+1][index];
+}
 
 main();
-
