305780f837546151a48a34879b0fda5e76764489
David Stafford <dstaff@10xgenomics.com>
2016-06-27 18:54:59 -0700

    Implement permalinks
    This involved a re-architect dynchart.js around a more OOP model

--- web/sere2/assets/scripts/10x/dynchart.js
+++ web/sere2/assets/scripts/10x/dynchart.js
@@ -8,11 +8,12 @@ var global_table_data;
 var global_compare;
 var global_metrics_db;
 var global_metrics_table;
-var global_project_name="Default.json"
 
+var global_view_state;
 
 
 function main() {
+	global_view_state = new(ViewState);
 	console.log("HELLO WORLD"); 
 	google.charts.load('current', {'packages':['corechart', 'table']});
 	google.charts.setOnLoadCallback(function() {
@@ -21,18 +22,20 @@ function main() {
 		global_table = new google.visualization.Table(document.getElementById('table1'));
 		global_compare = new google.visualization.Table(document.getElementById('compare1'));
 		global_metrics_table= new google.visualization.Table(document.getElementById('list1'));
-		google.visualization.events.addListener(global_metrics_table, 'select', metrics_list_click);
+		google.visualization.events.addListener(global_metrics_table, 'select', global_view_state.metrics_list_click);
 
-		pickwindow("table")
+		//pickwindow("table")
+		var p = getParameterByName("params");
+		if (p != null && p != "") {
+			global_view_state.ReconstituteFromURL(p);
+
+		}
+		global_view_state.render();
 	});
 	
-	$("#table").hide();
-	$("#compare").hide();
-	$("#plot").hide();
-	$("#help").hide();
-	clear_error_box();
 
 	setup_project_dropdown();
+
 }
 
 function project_dropdown_click(x) {
@@ -40,8 +43,7 @@ function project_dropdown_click(x) {
 	console.log(event)
 
 	//document.getElementById("metricset").value = event.target.textContent
-	global_project_name = event.target.textContent;
-	$("#project_cur").text(event.target.textContent);
+	changeproject(event.target.textContent);
 
 }
 
@@ -53,7 +55,6 @@ function setup_project_dropdown() {
 		for (var i = 0; i < data.length; i++) {
 
 			var ng = document.createElement('li');
-			var zz = data[i];
 			ng.textContent = data[i];
 			ng.onclick = project_dropdown_click;
 			console.log(ng.textContent);
@@ -62,37 +63,93 @@ function setup_project_dropdown() {
 	})
 }
 
-function getmet() {
-	return global_project_name;
+function changeproject(p) {
+	global_view_state.project = p;
+	update_model_from_ui();
+	global_view_state.render();
 }
 
-/*
- * This is called on a click to any of the main navication buttons. |w| is the name
- * of the nav button that was clicked.
- */
-function pickwindow(w) {
-	console.log(w)
+function changetablemode(mode) {
+	update_model_from_ui()
+	global_view_state.table_mode = mode;
+	global_view_state.render();
+
+}
+function pickwindow(mode) {
+	update_model_from_ui()
+	global_view_state.mode = mode;
+	global_view_state.render();
+}
+
+function update() {
+	update_model_from_ui()
+	global_view_state.render();
+
+}
+
+
+
+function ViewState() {
+	this.mode = "table";
+	this.table_mode = "";
+	this.where = "";
+	this.project = "Default.json";
+	this.compareidnew= null;
+	this.compareidold= null;
+	this.chartx = null;
+	this.charty = null;
+	this.sample_search = null;
+
+	return this;
+}
+
+
+ViewState.prototype.GetURL = function() {
+	var url = document.location;
+	var str = url.host + url.pathname + "?params=" +
+		encodeURIComponent(JSON.stringify(this));
+	return str;
+}
+ViewState.prototype.ReconstituteFromURL = function(p) {
+	var p = decodeURIComponent(p);
+
+	var parsed = JSON.parse(p);
+	
+	var ks = Object.keys(parsed);
+
+	for (var i = 0; i < ks.length; i++) {
+		this[ks[i]] = parsed[ks[i]]
+	}
+}
+
+
+
+ViewState.prototype.render = function() {
 	$("#table").hide();
 	$("#compare").hide();
 	$("#plot").hide();
 	$("#help").hide();
 	clear_error_box();
 
+	var w = this.mode;
+
 	/* Special logic for handling the compare button. If you don't have exactly two
 	 * rows selected, don't compare. If you have run row selected, redo the table view
 	 * with selecting rows with the same sampleid
 	 */
 	if (w == "compare") {
-		var selected = global_table.getSelection();
-		if (selected.length!=2) {
+		if (!this.compareidnew || !this.compareidold) {
 			set_error_box("Please select two rows to compare. Then click compare again.")
-			var wc=(get_data_at_row(global_table_data, "sampleid", selected[0].row));
-			document.getElementById("where").value="sampleid =" + wc;
+			//var wc=(get_data_at_row(global_table_data, "sampleid", selected[0].row));
+			if (this.compareidold) {
+				this.where = "sampleid=" + this.sample_search
+				console.log(this.where);
+			}
 
-			table_update();
+			this.table_update();
 			$("#table").show();
-		} else if (selected.length == 2){
-			compare_update();
+		} else{
+			this.compare_update();
 			$("#compare").show();
 		}
 	}
@@ -101,37 +158,41 @@ function pickwindow(w) {
 		$("#" + w).show();
 
 		if (w == "table") {
-			table_update();
+			this.table_update();
 		}
 
 		if (w == "plot") {
-			$.getJSON("/api/list_metrics?metrics_def=" + getmet(), function(data) {
+			$.getJSON("/api/list_metrics?metrics_def=" + this.project, function(data) {
 				global_metrics_db = data.ChartData;
 				var mdata = google.visualization.arrayToDataTable(global_metrics_db);
 				global_metrics_table.draw(mdata, {})
 			})
 
+			this.chart_update();
+
 		}
 	}
+	$("#project_cur").text(this.project);
+	$("#myurl").text(this.GetURL());
 }
 
 /*
  * Render the compare view */
-function compare_update() {
+ViewState.prototype.compare_update = function() {
 
-	var selected = global_table.getSelection();
-	console.log(selected)
-	var idold = get_data_at_row(global_table_data, "test_reports.id", selected[0].row);
-	var idnew= get_data_at_row(global_table_data, "test_reports.id", selected[1].row);
+	//var selected = global_table.getSelection();
+	//console.log(selected)
+	//var idold = get_data_at_row(global_table_data, "test_reports.id", selected[0].row);
+	//var idnew= get_data_at_row(global_table_data, "test_reports.id", selected[1].row);
 
 	
 	/*var url = "/api/compare?base=" + document.getElementById("idold").value +
 		"&new=" + document.getElementById("idnew").value +
 		"&metrics_def=met1.json"
 		*/
-	var url = "/api/compare?base=" + idold + 
-		"&new=" + idnew + 
-		"&metrics_def=" + getmet();
+	var url = "/api/compare?base=" + this.compareidold +
+		"&new=" + this.compareidnew+ 
+		"&metrics_def=" + this.project;
 	
 	console.log(url)
 	$.getJSON(url, function(data) {
@@ -143,22 +204,20 @@ function compare_update() {
 
 
 	})
-
-
 }
 
 /* Render the metric view */
-function table_update(mode) {
-	var where = encodeURIComponent(document.getElementById("where").value);
+ViewState.prototype.table_update = function()  { 
+	var where = this.where;
 
-
+	var mode = this.table_mode;
 	if (mode=="metrics") {
 		var url = "/api/plotall?where=" + where 
 	} else {
 		var url = "/api/plot?where=" + where + "&columns=test_reports.id,SHA,userid,finishdate,sampleid,comments"
 	}
 
-	url += "&metrics_def=" + getmet();
+	url += "&metrics_def=" + this.project;
 
 	$.getJSON(url, function(data) {
 		global_table_data = data;
@@ -172,7 +231,7 @@ function table_update(mode) {
 /*
  * Handle a click on the table of metrics in the chart page
  */
-function metrics_list_click() {
+ViewState.prototype.metrics_list_click = function() {
 	var y = document.getElementById("charty");
 	
 	var sel = global_metrics_table.getSelection();
@@ -185,27 +244,41 @@ function metrics_list_click() {
 
 		v = v  +global_metrics_db[idx+1];
 	}
-
-
 	y.value = v;
+}
+
+function update_model_from_ui() {
+	var v = global_view_state;
+	v.chartx = document.getElementById("chartx").value;
+	v.charty = document.getElementById("charty").value;
+	v.where = document.getElementById("where").value;
+	var selected = global_table.getSelection();
+	if (selected[0]) {
+		v.sample_search=(get_data_at_row(global_table_data, "sampleid", selected[0].row));
+
+		v.compareidold= get_data_at_row(global_table_data, "test_reports.id", selected[0].row);
+	} else {
+		v.compareidold = null;
+	}
+	if (selected[1]) {
+		v.compareidnew= get_data_at_row(global_table_data, "test_reports.id", selected[1].row);
+	} else {
+		v.compareidnew = null;
+	}
+
 }
 
 /*
  * Update the chart.
  */
-function chart_update() {
-	var x = document.getElementById("chartx");
-	var y = document.getElementById("charty");
-	var where = document.getElementById("where");
+ViewState.prototype.chart_update = function() {
+	var x = this.chartx;
+	var y = this.charty;
+	var where = this.where
 
-	console.log(x.value);
-	console.log(x);
-	console.log(y);
-	console.log(where);
-
-	var url = "/api/plot?where="+encodeURIComponent(where.value)+
-		"&columns=" + encodeURIComponent(x.value) + "," + encodeURIComponent(y.value) +
-		"&metrics_def=" + getmet();
+	var url = "/api/plot?where="+encodeURIComponent(where)+
+		"&columns=" + encodeURIComponent(x) + "," + encodeURIComponent(y) +
+		"&metrics_def=" + this.project;
 
 	console.log(url);
 	$.getJSON(url, function(data) {
@@ -275,4 +348,18 @@ function clear_error_box() {
 	$("#errorbox").hide();
 }
 
+
+/* Shamelessly stolen from stackoverflow */
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 main();
+
+
+
