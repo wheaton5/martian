ef6778d47e61415f806425012be6b16d9412716f
Alex Wong <alex@10xgenomics.com>
2016-07-30 11:02:58 -0700

    Enable full stage reset in MARSOC and SERE

--- src/martian/sere/main.go
+++ src/martian/sere/main.go
@@ -186,7 +186,7 @@ Options:
 	// Runtime
 	rt := core.NewRuntimeWithCores(jobMode, vdrMode, profileMode, martianVersion,
 		-1, -1, reqMemPerCore, maxJobs, jobFreqMillis, jobResources,
-		false, stackVars, zip, skipPreflight, enableMonitor, debug, false, "")
+		true, stackVars, zip, skipPreflight, enableMonitor, debug, false, "")
 
 	// Mailer
 	mailer := manager.NewMailer(instanceName, emailHost, emailSender,
